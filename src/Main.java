5df7143243771730d5711d9ec89acfc8f0153dd4
Nathlima1105 <nathaliasilvalima5@gmail.com>
2023-06-06 08:29:48 -0300

    Começamos a fazer as classes.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-
+       // Construir balões
+        Balao b1 = new Balao();
+        String resultado = b1.voar();
+        System.out.println(resultado);
     }
 }
