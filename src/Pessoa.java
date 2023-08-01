72e556cdb95070e1f85c6c5c5d6ae227b769a75b
Nathalia Silva Lima <nathaliasilvalima5@gmail.com>
2023-08-01 08:27:37 -0300

    Construindo.

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,9 +1,24 @@
 public class Pessoa {
 
     String nome;
+    String CPF;
+    String RG;
+    String telefone;
+    String telefoneFamilia;
+    String endereço;
+    boolean assinarTermo;
 
-    public Nome mostrarNome(){
-        Nome n1 = new Nome();
-        return n1;
+
+    public Pessoa(String nome, String CPF, String RG, String telefone, String telefoneFamilia, String endereço, boolean assinarTermo) {
+        this.nome = "Marcos";
+        this.CPF = "37530484885";
+        this.RG = "621349578";
+        this.telefone = "11949768967";
+        this.telefoneFamilia = "11967543316";
+        this.endereço = "Baruaeri Parque dos camargos Rua marcela 333";
+        this.assinarTermo = true;
+
     }
+
 }
+
---
+++ src/Piloto.java
@@ -0,0 +1,19 @@
+public class Piloto {
+
+    String nome;
+
+    String CPF;
+
+    String telefone;
+
+    String numANAC;
+
+    public String piloto(){return "pilotando";}
+
+    public Piloto(String nome, String CPF, String telefone,String numANAC ) {
+        this.nome = "Marcos";
+        this.CPF = "37530484885";
+        this.telefone = "11949768967";
+        this.numANAC = "45367899385675678";
+    }
+}
